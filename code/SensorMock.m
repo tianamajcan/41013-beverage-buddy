39ac323aea8e9feb806e1bda99afba4f7cadba6e
gameboxthing <quentin.l.personal@gmail.com>
2022-05-02 17:25:48 +1000

    added position getting to the sensor mock

--- code/SensorMock.m
+++ code/SensorMock.m
@@ -1,10 +1,22 @@
 classdef SensorMock < handle
     properties
-        beverage_pos;
+        beverages;
     end
     
     methods
+        function self = SensorMock(bevvys)
+            % constructor for the mock sensor class
+            self.beverages = bevvys;
+        end
         
-        
+        % getters
+        function r = getBeveragePositions(self)
+            positions = {}; %zeros(size(self.beverages));
+            for i = 1:size(self.beverages')
+                positions{i} = self.beverages(i).pose;
+            end
+            
+            r = positions;
+        end
     end
 end
