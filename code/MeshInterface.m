a88505d3502ccb1f3a336925d40d7b1023ff3ea7
gameboxthing <quentin.l.personal@gmail.com>
2022-05-02 16:04:24 +1000

    filled out the mesh interface class. still need to do scale if possible

---
+++ code/MeshInterface.m
@@ -0,0 +1,59 @@
+% note this might not be the best way to do this, however it is a start at
+% producing an interface class for all objects that are added to the
+% simulated environment
+
+classdef MeshInterface < handle
+    % an interface class that forms the basis of environment objects.
+    % (meshes)
+    
+    properties
+        model;  % the object mesh
+        vertices;  % vertices at the object pose
+        pose;  % the pose of the object
+    end
+    
+    methods
+        function self = ObjectInterface(object_file, initial_pose)
+            % constructor for the environment interface
+            % accepts an initial pose and an object file to generate
+            
+            arguments
+                object_file;
+                initial_pose = eye(4);  % default position
+            end
+            self.model = PlaceObject(object_file);
+            self.vertices = get(self.model, 'Vertices');
+            self.updatePose(initial_pose);
+        end
+        
+        function delete(self)
+            %  deletes the object
+            delete(self.model);
+        end
+        
+        % setters
+        function updatePose(self, new_pose)
+            % updates the pose of the object
+            trVertices = [self.vertices, ones(size(self.vertices, 1),1)] * new_pose';
+            self.pose = transform;
+            set(self.model, 'Vertices', trVertices(:,1:3));
+        end
+        
+        function setScale(self, scale)
+            % sets/alters the scale of the objects mesh
+            % TODO: is this possible, haven't attempted yet
+        end
+        
+        % getters
+        function r = getPose(self)
+            % gets the pose of the object
+            r = self.pose;
+        end
+        
+        function r = getModel(self)
+            % gets the objects model
+            r = self.model;
+        end
+        
+    end
+end
