90c778374ebde2f90c67525b7b22e36c3a39f79a
tianamajcan <tiana.majcan@gmail.com>
2022-05-13 16:23:41 +1000

    Added coaster

--- code/functions/environmentSetup.m
+++ code/functions/environmentSetup.m
@@ -26,6 +26,9 @@ table = MeshInterface('environment_assets/table.ply', se3(se2(-1, 0, pi/2)));
 % add chair
 chair = MeshInterface('chair.ply', se3(se2(0.25, 0.75, -pi/2)));
 
+% add coaster (where the drink must be placed)
+coaster = MeshInterface('coaster.ply', transl(-0.75,0.1,0.78));
+
 %add rubbish bin
 redbin = MeshInterface('redbin_rubbish.ply', transl(-0.4,-1.5,0));
 
@@ -33,7 +36,7 @@ redbin = MeshInterface('redbin_rubbish.ply', transl(-0.4,-1.5,0));
 yellowbin = MeshInterface('yellowbin_recycling.ply', transl(-1,-1.5,0));
 
 % add fridge
-fridgeBase = MeshInterface('fridge_base.ply', transl(-1.3, -0.6, 0.75)*trotz(pi/2));
+fridgeBase = MeshInterface('fridge_base.ply', transl(-1.3, -0.6, 0.78)*trotz(pi/2));
 % fridgeDoor = MeshInterface('fridge_door.ply', transl(-0.85, -1.75, 0)*trotz(pi/2));
 
 % add some drinks in the fridge
@@ -53,7 +56,7 @@ for i = 1:3
     drinks{i, 4} = Drink('beer', transl((shelf(1)-0.2)+(0.1*i), shelf(2)+0.2, shelf(3)));
 end
 
-objects = {table, chair, fridgeBase, yellowbin, redbin};
+objects = {table, chair, fridgeBase, coaster, yellowbin, redbin};
 
 end
 
