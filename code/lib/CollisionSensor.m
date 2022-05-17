c8236f968e47cdb96e4ef33886f5f75732a1e096
tianamajcan <tiana.majcan@gmail.com>
2022-05-18 02:16:57 +1000

    Updated with collision detection

--- code/lib/CollisionSensor.m
+++ code/lib/CollisionSensor.m
@@ -22,12 +22,55 @@ classdef CollisionSensor < SensorMock
 
         end
         
-        function [robots, objects] = getSensorResult(self)
+        function r, robot, object = getSensorResult(self, robots, objects)
             %METHOD1 Checks the proximity of if any of the robots are close
-            % to collision with the objects
-           
+            % to collision with the objects, where r = 1 means an upcoming
+            % collision has been detected and r = 0 means no incoming
+            % collisions detected
+
+            % loop through the robots current in the scene
+            for i = length(robots)
+                n = robots{i}.robot.links;                  % num links
+                joints = robots{i}.getJointTransforms();    %transforms of the joins
+                
+                % loop through every object detected by the sensor
+                for j = 1: length(objects)
+                    vertices = objects{j}.vertices;
+                    
+                    % loops through every link (including the base, i.e.
+                    % every ellipsoid) EXCEPT the end effector for the
+                    % purposes of this simulation 
+                    for k = 1:n
+                        % add column of ones to be allow calculating the transform the object points
+                        verticesAndOnes = [inv(joints{k}) * [vertices,ones(size(vertices,1),1)]']';
+                        updatedVertices = verticesAndOnes(:,1:3);   % remove column of ones after trasnform
+                        % return the (normalised) distance of a set of
+                        % points from the center of the ellipsoid. If the
+                        % distance is > 1 then the point is not within the
+                        % sphere, and if its greater than it is
+                        dist = getAlgebraicDist(updatedVertices, robots{i}.ellipsoid_centres(k,:), robots{i}.ellipsoid_radii(k,:));
+                        pointsInside = find(dist<1);
+                        
+                        if pointsInside > 1
+                            % return true
+                            r = 1;
+                            robot = robots{i}
+                            object = objects{j}
+                            disp(sprintf("WARNING: PERFORMING EMERGENCY STOP TO AVOID COLLISION!\n" + ...
+                                "Upcoming collision detected on the %dth link of robot %s " + ...
+                                "with object at location %.3f, %.3f, %.3f", n, robot.robot.name, object.pose(1,4), object.pose(2,4), object.pose(3,4))
+                            return
+                        else
+                            r = 0;
+                            robot = nan;
+                            objects = nan;
+                        end
+                    end
+                end
+            end
         end
 
     end
+
 end
 
